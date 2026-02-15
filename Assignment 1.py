5dc16a75a91f6d63df1942024497554c39e296ae
Ritu Sarkar <154949316+Rrain-0@users.noreply.github.com>
2026-02-15 17:56:17 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,25 @@
+print("Task 1: Perform Basic Mathematical Operation")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter Second number: "))
+Addition = num1 + num2
+Substraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+print("""
+Addition: """, Addition)
+print("Substraction: ", Substraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
+print("End of Task1")
+
+print("""
+
+Task 2: Create a personalized Greeting""")
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+name3 = name2 + '!'
+print("Hello,", name1,name3,"welcome to the python program.")
+print("End of Task2")
+
+
+
